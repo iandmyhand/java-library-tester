be4e93259e515d625e613207a11eb0f50e981181
SeomGi, Han <iandmyhand@gmail.com>
2015-07-03 11:31:57 +0900

    Update to use the Logger instead of the System.out.println().

--- java-library-tester/src/test/java/study/hard/javalib/nativelib/LanguageTest.java
+++ java-library-tester/src/test/java/study/hard/javalib/nativelib/LanguageTest.java
@@ -1,22 +1,31 @@
 package study.hard.javalib.nativelib;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import study.hard.javalib.commons.entity.BasisObject;
 
 public class LanguageTest {
+
+	private static Logger logger = LoggerFactory.getLogger(LanguageTest.class);
 
 	@Test
 	public void testStaticVariable() {
 		ClassThatHasStaticVariable a1 = new ClassThatHasStaticVariable();
+		logger.debug("a1: " + a1);
 		ClassThatHasStaticVariable a2 = new ClassThatHasStaticVariable();
+		logger.debug("a2: " + a2);
 	}
 
-}
+	private static class ClassThatHasStaticVariable extends BasisObject {
+		private static final long serialVersionUID = -8833377195166280242L;
 
-class ClassThatHasStaticVariable {
-	static int count = 0;
+		static int count = 0;
 
-	ClassThatHasStaticVariable() {
-		count++;
-		System.out.print(count);
+		ClassThatHasStaticVariable() {
+			count++;
+			logger.debug("count: " + count);
+		}
 	}
 }
