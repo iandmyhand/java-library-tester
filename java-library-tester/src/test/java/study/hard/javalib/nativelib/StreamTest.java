59c51a1af7ee3885d9c07ae293cab09f44787a27
SeomGi, Han <iandmyhand@gmail.com>
2016-05-31 13:39:58 +0900

    Add test case related to Stream API of JDK8.

--- java-library-tester/src/test/java/study/hard/javalib/nativelib/StreamTest.java
+++ java-library-tester/src/test/java/study/hard/javalib/nativelib/StreamTest.java
@@ -1,7 +1,49 @@
 package study.hard.javalib.nativelib;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
 /**
  * Created by admin on 5/31/16.
  */
 public class StreamTest {
+
+    @Test
+    public void streamTest() {
+        List<Integer> myList = new ArrayList<>();
+        for(int i=0; i<10; i++) myList.add(i);
+
+        //traversing using Iterator
+        Iterator<Integer> it = myList.iterator();
+        while(it.hasNext()){
+            Integer i = it.next();
+            System.out.println("Iterator Value::"+i);
+        }
+
+        //traversing through forEach method of Iterable with anonymous class
+        myList.forEach(new Consumer<Integer>() {
+
+            public void accept(Integer t) {
+                System.out.println("forEach anonymous class Value::"+t);
+            }
+
+        });
+
+        //traversing with Consumer interface implementation
+        MyConsumer action = new MyConsumer();
+        myList.forEach(action);
+    }
+
+    //Consumer implementation that can be reused
+    private class MyConsumer implements Consumer<Integer>{
+
+        public void accept(Integer t) {
+            System.out.println("Consumer impl Value::"+t);
+        }
+        
+    }
 }
--- java-library-tester/src/test/java/study/hard/javalib/nativelib/StringTest.java
+++ java-library-tester/src/test/java/study/hard/javalib/nativelib/StringTest.java
@@ -10,45 +10,45 @@ import com.google.common.collect.Lists;
 
 public class StringTest {
 
-	static final String STR_WITH_LF = "one\ntwo\nthree";
-	static final String STR_WITH_LF_AND_EMPTYLINE = "one\ntwo\nthree\n";
-	static final String STR_WITH_CRLF = "one\r\ntwo\r\nthree";
-	static final String STR_WITH_CRLF_AND_EMPTYLINE = "one\r\ntwo\r\nthree\r\n\r\n";
-	static final String STR_SAMPLE_HANGUL = "한글 샘플 문자열";
+    static final String STR_WITH_LF = "one\ntwo\nthree";
+    static final String STR_WITH_LF_AND_EMPTYLINE = "one\ntwo\nthree\n";
+    static final String STR_WITH_CRLF = "one\r\ntwo\r\nthree";
+    static final String STR_WITH_CRLF_AND_EMPTYLINE = "one\r\ntwo\r\nthree\r\n\r\n";
+    static final String STR_SAMPLE_HANGUL = "한글 샘플 문자열";
 
-	@Test
-	public void testToSplitNewLine() {
-		List<String> strList;
+    @Test
+    public void testToSplitNewLine() {
+        List<String> strList;
 
-		strList = Lists.newArrayList(STR_WITH_LF.split("\\r?\\n"));
-		assertEquals(3, strList.size());
-		System.out.println(strList);
+        strList = Lists.newArrayList(STR_WITH_LF.split("\\r?\\n"));
+        assertEquals(3, strList.size());
+        System.out.println(strList);
 
-		strList = Lists.newArrayList(STR_WITH_CRLF.split("\\r?\\n"));
-		assertEquals(3, strList.size());
-		System.out.println(strList);
+        strList = Lists.newArrayList(STR_WITH_CRLF.split("\\r?\\n"));
+        assertEquals(3, strList.size());
+        System.out.println(strList);
 
-		strList = Lists.newArrayList(STR_WITH_LF_AND_EMPTYLINE.split("\\r?\\n"));
-		assertEquals(3, strList.size());
-		System.out.println(strList);
+        strList = Lists.newArrayList(STR_WITH_LF_AND_EMPTYLINE.split("\\r?\\n"));
+        assertEquals(3, strList.size());
+        System.out.println(strList);
 
-		strList = Lists.newArrayList(STR_WITH_CRLF_AND_EMPTYLINE.split("\\r?\\n"));
-		assertEquals(3, strList.size());
-		System.out.println(strList);
-	}
+        strList = Lists.newArrayList(STR_WITH_CRLF_AND_EMPTYLINE.split("\\r?\\n"));
+        assertEquals(3, strList.size());
+        System.out.println(strList);
+    }
 
-	@Test
-	public void testSubstring() {
-		String tmp = STR_SAMPLE_HANGUL;
-		System.out.println("tmp: [" + tmp + "]");
-		System.out.println("tmp length: " + tmp.length());
-		tmp = tmp.substring(0, 5);
-		System.out.println("tmp: [" + tmp + "]");
-	}
-	
-	@Test
-	public void testFormating() {
-		System.out.println(String.format("A color of %s is the %s.", "Banana", "yellow"));
-	}
+    @Test
+    public void testSubstring() {
+        String tmp = STR_SAMPLE_HANGUL;
+        System.out.println("tmp: [" + tmp + "]");
+        System.out.println("tmp length: " + tmp.length());
+        tmp = tmp.substring(0, 5);
+        System.out.println("tmp: [" + tmp + "]");
+    }
+
+    @Test
+    public void testFormatting() {
+        System.out.println(String.format("A color of %s is the %s.", "Banana", "yellow"));
+    }
 
 }
